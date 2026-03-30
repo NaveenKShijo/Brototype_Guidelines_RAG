4d01778068c1244a686fd8755bee024c5da661ba
Naveen <naveenshijo@gmail.com>
2026-03-30 10:12:19 +0530

    small change in document reader code

--- src/ingest.py
+++ src/ingest.py
@@ -1,12 +1,22 @@
 from llama_index.core import SimpleDirectoryReader
+from llama_index.readers.file import PDFReader
 from llama_index.core.node_parser import SentenceSplitter
 from src.config import DATA_DIR, CHUNK_SIZE, CHUNK_OVERLAP
 
 def load_documents():
     """
-        Load data sources and convert them into documents
+        Load data sources and convert them into documents using an explicit PDF reader
     """
-    documents = SimpleDirectoryReader(DATA_DIR).load_data()
+
+    # documents = SimpleDirectoryReader(DATA_DIR).load_data()
+    # it failed as it downgraded to return raw pdf structure instead of readable text.
+    # So we explicitly use PDFReader.
+
+    loader = SimpleDirectoryReader(
+        DATA_DIR,
+        file_extractor={".pdf": PDFReader()}
+    )
+    documents = loader.load_data()
     return documents
 
 def chunk_documents(documents):
